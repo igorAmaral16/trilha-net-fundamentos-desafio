828e35a85a5c0d30e5bc2386106ae48c15b312ae
IgorAmaral16 <igoramaraal089@gmail.com>
2023-12-27 22:31:35 -0300

    Desafios concluídos. Adicionais: Tratamneto de erros; Melhor flexibilidade;

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,6 +16,39 @@ namespace DesafioFundamentos.Models
         {
             // Implementado!!!!!
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string veiculoParaAdicionar = Console.ReadLine().ToUpper();
+
+            if (veiculoParaAdicionar.Count() == 7)
+            {
+                string pegar3PrimeirosCaracteres = veiculoParaAdicionar.Substring(0, 3);
+                string pegar4UltimosCaracteres = veiculoParaAdicionar.Substring(3, 4);
+
+                if (veiculos.Contains(veiculoParaAdicionar))
+                {
+                    Console.WriteLine("Veículo já adicionado anteriormente.");
+                }
+                else if (VerificarLetra(pegar3PrimeirosCaracteres) == false
+                        || VerificarNumero(pegar4UltimosCaracteres) == false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Placa: {veiculoParaAdicionar} inválida, por favor, insira uma placa válida.\n");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    veiculos.Add(veiculoParaAdicionar);
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"O veículo {veiculoParaAdicionar.Substring(0, 3)}-{veiculoParaAdicionar.Substring(3, 4)} foi cadastrado com sucesso.\n");
+                    Console.ResetColor();
+                }
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Placa: {veiculoParaAdicionar} inválida, por favor, insira uma placa válida.\n");
+                Console.ResetColor();
+            }
+
         }
 
         public void RemoverVeiculo()
@@ -24,27 +57,46 @@ namespace DesafioFundamentos.Models
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
             // *IMPLEMENTE AQUI*
-            string placa = "";
+            string placa = Console.ReadLine().ToUpper();
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (veiculos.Any(x => x.ToUpper() == placa))
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = 0;
-                decimal valorTotal = 0; 
+                string horasParaConverter = Console.ReadLine();
+
+                bool conversaoComSucesso = int.TryParse(horasParaConverter, out int horas);
+                decimal valorTotal = 0.0M;
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
 
-                Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
+                if (conversaoComSucesso)
+                {
+                    valorTotal = precoInicial + precoPorHora * horas;
+                    veiculos.Remove(placa);
+
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"O veículo: {placa.Substring(0, 3)}-{placa.Substring(3, 4)} foi removido com sucesso. Valor total a ser pago: {valorTotal:C}.");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"O valor '{horasParaConverter}' não é válido, por favor, digite uma quantidade válida.");
+                    Console.ResetColor();
+                }
+
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Desculpe, esse veículo não está estacionado aqui. Confira se digitou a placa corretamente");
+                Console.ResetColor();
             }
         }
 
@@ -56,11 +108,47 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+                Console.ForegroundColor = ConsoleColor.DarkBlue;
+                foreach (string veiculo in veiculos)
+                {
+                    Console.WriteLine("\t      _______       ");
+                    Console.WriteLine("\t    /_|_____|_\\    ");
+                    Console.WriteLine("\t o-(   o   o   )-o  ");
+                    Console.WriteLine("\t    `-._____,-'     ");
+                    Console.WriteLine($"\t   | {veiculo.Substring(0, 3)}-{veiculo.Substring(3, 4)} |   ");
+                    Console.WriteLine("\t____________________");
+
+                }
+                Console.ResetColor();
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
                 Console.WriteLine("Não há veículos estacionados.");
+                Console.ResetColor();
             }
+        }
+        public static bool VerificarLetra(string texto)
+        {
+            foreach (char letra in texto)
+            {
+                if (!char.IsLetter(letra))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static bool VerificarNumero(string texto)
+        {
+            foreach (char letra in texto)
+            {
+                if (!char.IsDigit(letra))
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -23,11 +23,17 @@ bool exibirMenu = true;
 while (exibirMenu)
 {
     Console.Clear();
-    Console.WriteLine("Digite a sua opção:");
-    Console.WriteLine("1 - Cadastrar veículo");
-    Console.WriteLine("2 - Remover veículo");
-    Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    Console.WriteLine("╔═════════════ Menu ══════════════╗");
+    Console.WriteLine("║ Digite a sua opção:             ║");
+    Console.WriteLine("║ 1 - Cadastrar veículo           ║");
+    Console.WriteLine("║ 2 - Remover veículo             ║");
+    Console.WriteLine("║ 3 - Listar veículos             ║");
+    Console.WriteLine("║ 4 - Encerrar                    ║");
+    Console.WriteLine("╚═════════════════════════════════╝");
+    Console.ResetColor();
+
 
     switch (Console.ReadLine())
     {
